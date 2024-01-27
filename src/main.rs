6e94f81339592f4afde238488c7bc0ef02e9cf38
yujiteshima <yuji.teshima@gmail.com>
2024-01-27 17:05:34 +0900

    add print!

--- src/main.rs
+++ src/main.rs
@@ -20,5 +20,7 @@ fn main() {
     )
     .get_matches();
 
-  println!("{:#?}", matches);
+  let text = matches.values_of_lossy("text").unwrap();
+  let omit_newline = matches.is_present("omit_newline");
+  print!("{}{}", text.join(" "), if omit_newline { " " } else { "\n" });
 }
